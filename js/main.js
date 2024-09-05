cdb4b0a95e2b8f02be27a5140aee235a994b42de
lby132 <quddbscjswow@naver.com>
2024-09-05 15:39:27 +0900

    헤더 추가 작업

--- js/main.js
+++ js/main.js
@@ -14,3 +14,21 @@ searchInputEl.addEventListener('blur', function(){
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+// _.throttle() 함수 : lodash 자바스크립트 라이브러리 사용
+window.addEventListener('scroll', _.throttle(function(){
+  console.log(window.scrollY);
+  if (window.scrollY > 500){
+    // 배지 숨기기
+    // gsap 자바스크립트 라이브러리 사용
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0
+    });
+  } else {
+    // 배지 보이기
+    badgeEl.style.display = 'block';
+  }
+}, 300));
