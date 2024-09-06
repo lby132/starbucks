b93a2411b8a2df6c50d7aedc6f959f6e4e704d22
lby132 <quddbscjswow@naver.com>
2024-09-06 14:42:45 +0900

    이미지 슬라이드 적용

--- js/main.js
+++ js/main.js
@@ -25,10 +25,47 @@ window.addEventListener('scroll', _.throttle(function(){
     // gsap 자바스크립트 라이브러리 사용
     // gsap.to(요소, 지속시간, 옵션);
     gsap.to(badgeEl, .6, {
-      opacity: 0
+      opacity: 0,
+      display: 'none'
     });
   } else {
     // 배지 보이기
-    badgeEl.style.display = 'block';
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
   }
 }, 300));
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 7,
+    opacity: 1
+  })
+});
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+});
+new Swiper('.promotion .swiper-container', {
+  // direction: 'horizontal'  horizontal 가 기본값임
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true, // 슬라이드를 넘겼을때 끝나지 않고 다시 첫번째 슬라이드를 보여주면서 반복적으로 나타냄
+  autoplay: {
+    delay: 5000
+  }, // 자동재생 기능
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
